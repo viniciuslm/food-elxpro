283449a39b53426cec378455ab0dec3ccc1d354d
Vinicius Moreira <viniciuslm@gmail.com>
2022-03-27 10:37:34 -0300

    aula sobre component - part 2

--- lib/food_elxpro_web/live/first/first_live.ex
+++ lib/food_elxpro_web/live/first/first_live.ex
@@ -3,17 +3,18 @@ defmodule FoodElxproWeb.FirstLive do
   alias FoodElxproWeb.First.IncComponent
 
   def mount(_assign, _session, socket) do
-    components = [1,2,3,4,5]
-    total = components |> Enum.sum
-    {:ok, socket |> assign(components: components, total: total)}
+    components = [1, 2, 3, 4, 5]
+    total = components |> Enum.sum()
+    component = 1
+    {:ok, socket |> assign(components: components, total: total, component: component)}
   end
 
   def handle_info(:inc_parent, socket) do
     {:noreply, socket |> update(:total, fn x -> x + 1 end)}
   end
 
-  def handle_event("save", %{"component" => component}, socket) do
+  def handle_event("save", %{"form" => %{"component" => component}}, socket) do
     send_update(IncComponent, id: "inc-" <> component, inc: 1)
-    {:noreply, socket |> update(:total, fn x -> x + 1 end)}
+    {:noreply, socket |> update(:total, fn x -> x + 1 end) |> update(:component, fn _ -> component end)}
   end
 end
