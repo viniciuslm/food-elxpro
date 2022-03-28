c451dc6e5e18522e94543b53cdb81d9aa79654cd
Vinicius Moreira <viniciuslm@gmail.com>
2022-03-27 21:17:42 -0300

    format

--- lib/food_elxpro_web/live/main/main_live.ex
+++ lib/food_elxpro_web/live/main/main_live.ex
@@ -2,6 +2,6 @@ defmodule FoodElxproWeb.MainLive do
   use FoodElxproWeb, :live_view
 
   def mount(_assigns, _session, socket) do
-    {:ok, socket |>  assign(name: "Vinicius", age: 44)}
+    {:ok, socket |> assign(name: "Vinicius", age: 44)}
   end
 end
