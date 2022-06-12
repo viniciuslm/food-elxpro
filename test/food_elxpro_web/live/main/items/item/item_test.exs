d32e35c0f46610ff4af6998cfb2aedfe2ce67088
Vinicius Moreira <viniciuslm@gmail.com.br>
2022-06-12 07:43:10 -0300

    create test

--- test/food_elxpro_web/live/main/items/item/item_test.exs
+++ test/food_elxpro_web/live/main/items/item/item_test.exs
@@ -1,15 +1,29 @@
 defmodule FoodElxproWeb.Main.ItemTest do
   use FoodElxproWeb.ConnCase
   import Phoenix.LiveViewTest
+  import FoodElxpro.Factory
+
   alias FoodElxproWeb.Router.Helpers, as: Routes
 
   test "load item", %{conn: conn} do
+    product = insert(:product)
     {:ok, view, _html} = live(conn, Routes.main_path(conn, :index))
-    assert has_element?(view, "#item-1")
-    assert has_element?(view, "[data-role=product-img][data-id=item-1]")
+    assert has_element?(view, "#item-#{product.id}")
+    assert has_element?(view, "[data-role=product-img][data-id=item-#{product.id}]")
     assert has_element?(view, "[data-role=product-description]")
-    assert has_element?(view, "[data-role=product-name][data-id=item-1]", "Produto com Nome")
-    assert has_element?(view, "[data-role=product-price][data-id=item-1]", "$ 10")
-    assert has_element?(view, "[data-role=product-add][data-id=item-1]")
+
+    assert has_element?(
+             view,
+             "[data-role=product-name][data-id=item-#{product.id}]",
+             product.name
+           )
+
+    assert has_element?(
+             view,
+             "[data-role=product-price][data-id=item-#{product.id}]",
+             Money.to_string(product.price)
+           )
+
+    assert has_element?(view, "[data-role=product-add][data-id=item-#{product.id}]")
   end
 end
