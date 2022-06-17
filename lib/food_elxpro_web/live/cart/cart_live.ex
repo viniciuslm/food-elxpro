cb21671196eb5f4d5b1b9cc4746f2b543139679c
Vinicius Moreira <viniciuslm@gmail.com.br>
2022-06-17 20:32:29 -0300

    codificando cart

--- lib/food_elxpro_web/live/cart/cart_live.ex
+++ lib/food_elxpro_web/live/cart/cart_live.ex
@@ -2,8 +2,11 @@ defmodule FoodElxproWeb.CartLive do
   use FoodElxproWeb, :live_view
 
   alias __MODULE__.{CartDetail, EmptyCart}
+  alias FoodElxpro.Carts
 
   def mount(_p, _s, socket) do
-    {:ok, socket}
+    cart_id = socket.assigns.cart_id
+    cart = Carts.get(cart_id)
+    {:ok, assign(socket, cart: cart)}
   end
 end
--- lib/food_elxpro_web/live/main/items/item/item.ex
+++ lib/food_elxpro_web/live/main/items/item/item.ex
@@ -1,4 +1,16 @@
 defmodule FoodElxproWeb.Main.Items.Item do
   use FoodElxproWeb, :live_component
-  alias FoodElxpro.Products
+  alias FoodElxpro.{Carts, Products}
+
+  def handle_event("add", _, socket) do
+    product = socket.assigns.product
+    cart_id = socket.assigns.cart_id
+    add_product_to_cart(cart_id, product)
+
+    {:noreply, socket |> put_flash(:info, "Item add to cart") |> push_redirect(to: "/")}
+  end
+
+  defp add_product_to_cart(cart_id, product) do
+    Carts.add(cart_id, product)
+  end
 end
