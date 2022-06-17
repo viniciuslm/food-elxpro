3d72986108a24bb86accdc3fb4069525cef6a353
Vinicius Moreira <viniciuslm@gmail.com.br>
2022-06-17 19:28:06 -0300

    add hook cart

---
+++ assets/js/hooks/cartSession.js
@@ -0,0 +1,12 @@
+const CartSession = {
+    mounted() {
+        this.handleEvent("create-cart-session-id", map => {
+            var {
+                cartId: cartId
+            } = map
+            sessionStorage.setItem("cart_id", cartId)
+        })
+    },
+}
+
+export default CartSession;
---
+++ lib/food_elxpro_web/live/live_sessions/cart.ex
@@ -0,0 +1,44 @@
+defmodule LiveSessions.Cart do
+  import Phoenix.LiveView
+  alias FoodElxpro.{Accounts, Carts}
+
+  def on_mount(:default, _params, session, socket) do
+    cart_id = get_connect_params(socket)["cart_id"]
+    socket = socket |> assign_user(session["user_token"]) |> create_cart(cart_id)
+    {:cont, socket}
+  end
+
+  defp assign_user(socket, nil), do: assign_new(socket, :current_user, nil)
+
+  defp assign_user(socket, user_token) do
+    assign_new(socket, :current_user, fn ->
+      Accounts.get_user_by_reset_password_token(user_token)
+    end)
+  end
+
+  defp create_cart(socket, cart_id) do
+    current_user = socket.assigns.current_user
+
+    cart_id = build_cart_id(current_user, cart_id)
+
+    socket
+    |> assign(cart_id: cart_id)
+    |> push_event("create-cart-session-id", %{"cartId" => cart_id})
+  end
+
+  defp build_cart_id(nil, nil) do
+    cart_id = Ecto.UUID.generate()
+    Carts.create(cart_id)
+    cart_id
+  end
+
+  defp build_cart_id(nil, cart_id) do
+    Carts.create(cart_id)
+    cart_id
+  end
+
+  defp build_cart_id(%{id: cart_id}, _cart_id) do
+    Carts.create(cart_id)
+    cart_id
+  end
+end
