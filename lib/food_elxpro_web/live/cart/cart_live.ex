ffb8db41ee9c04ce9a8ba025dd8a71b6f36e1764
Vinicius Moreira <viniciuslm@gmail.com.br>
2022-06-17 17:42:50 -0300

    frontend cart

--- lib/food_elxpro_web/live/cart/cart_live.ex
+++ lib/food_elxpro_web/live/cart/cart_live.ex
@@ -1,7 +1,7 @@
 defmodule FoodElxproWeb.CartLive do
   use FoodElxproWeb, :live_view
 
-  alias __MODULE__.EmptyCart
+  alias __MODULE__.{CartDetail, EmptyCart}
 
   def mount(_p, _s, socket) do
     {:ok, socket}
