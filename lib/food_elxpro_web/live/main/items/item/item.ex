e38bfa71198f3e70907f0ff22a4e053528ddc598
Vinicius Moreira <viniciuslm@gmail.com.br>
2022-06-18 08:20:16 -0300

    find error

--- lib/food_elxpro_web/live/main/items/item/item.ex
+++ lib/food_elxpro_web/live/main/items/item/item.ex
@@ -1,4 +1,17 @@
 defmodule FoodElxproWeb.Main.Items.Item do
   use FoodElxproWeb, :live_component
+  alias FoodElxpro.Carts
   alias FoodElxpro.Products
+
+  def handle_event("add", _, socket) do
+    product = socket.assigns.product
+    cart_id = socket.assigns.cart_id
+    add_product_to_cart(cart_id, product)
+
+    {:noreply, socket |> put_flash(:info, "Item added to cart") |> push_redirect(to: "/")}
+  end
+
+  defp add_product_to_cart(cart_id, product) do
+    Carts.add(cart_id, product)
+  end
 end
