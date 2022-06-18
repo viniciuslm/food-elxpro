6b1a8ddf7b2b2ed926e67ed6b31120f61eaea9e3
Vinicius Moreira <viniciuslm@gmail.com.br>
2022-06-18 08:56:16 -0300

    finalizando hook cart

--- lib/food_elxpro_web/live/cart/cart_live.ex
+++ lib/food_elxpro_web/live/cart/cart_live.ex
@@ -2,8 +2,15 @@ defmodule FoodElxproWeb.CartLive do
   use FoodElxproWeb, :live_view
 
   alias __MODULE__.{CartDetail, EmptyCart}
+  alias FoodElxpro.Carts
 
   def mount(_p, _s, socket) do
-    {:ok, socket}
+    cart_id = socket.assigns.cart_id
+    cart = Carts.get(cart_id)
+    {:ok, assign(socket, cart: cart)}
+  end
+
+  def handle_info({:update, cart}, socket) do
+    {:noreply, assign(socket, cart: cart)}
   end
 end
