7b8c2e192330277b49ec07629ebe6bed176ad777
Vinicius Moreira <viniciuslm@gmail.com.br>
2022-06-24 15:48:55 -0300

    manipular sessao

--- lib/food_elxpro_web/live/live_sessions/cart.ex
+++ lib/food_elxpro_web/live/live_sessions/cart.ex
@@ -2,12 +2,12 @@ defmodule LiveSessions.Cart do
   import Phoenix.LiveView
 
   alias FoodElxpro.Accounts
-  alias FoodElxpro.Carts
+  alias LiveSessions.CreateCart
 
   def on_mount(:default, _, session, socket) do
     cart_id = get_connect_params(socket)["cart_id"]
     user_token = session["user_token"]
-    socket = socket |> assign_user(user_token) |> create_cart(cart_id)
+    socket = socket |> assign_user(user_token) |> CreateCart.execute(cart_id)
     {:cont, socket}
   end
 
@@ -19,29 +19,5 @@ defmodule LiveSessions.Cart do
       |> Accounts.get_user_by_session_token()
 
     assign_new(socket, :current_user, fn -> current_user end)
-  end
-
-  defp create_cart(socket, cart_id) do
-    current_user = socket.assigns.current_user
-
-    cart_id = build_cart_id(current_user, cart_id)
-
-    socket
-    |> assign(cart_id: cart_id)
-    |> push_event("create-cart-session-id", %{"cartId" => cart_id})
-  end
-
-  defp build_cart_id(nil, nil) do
-    cart_id = Ecto.UUID.generate()
-    return_cart_id(cart_id)
-  end
-
-  defp build_cart_id(nil, cart_id), do: return_cart_id(cart_id)
-
-  defp build_cart_id(%{id: cart_id}, _cart_id), do: return_cart_id(cart_id)
-
-  defp return_cart_id(cart_id) do
-    Carts.create(cart_id)
-    cart_id
   end
 end
--- lib/food_elxpro_web/live/live_sessions/permissions.ex
+++ lib/food_elxpro_web/live/live_sessions/permissions.ex
@@ -3,12 +3,32 @@ defmodule LiveSessions.Permissions do
 
   alias FoodElxpro.Accounts
   alias FoodElxproWeb.Router.Helpers, as: Routes
+  alias LiveSessions.CreateCart
 
   def on_mount(:admin, _params, %{"user_token" => user_token}, socket) do
     assign_user(socket, :admin, user_token)
   end
 
+  def on_mount(:user, _params, %{"user_token" => user_token}, socket) do
+    assign_user(socket, :user, user_token)
+  end
+
   defp assign_user(socket, _, nil), do: error_login(socket, "You must be logged in")
+
+  defp assign_user(socket, :user, user_token) do
+    current_user =
+      user_token
+      |> Accounts.get_user_by_session_token()
+
+    cart_id = get_connect_params(socket)["cart_id"]
+
+    socket =
+      socket
+      |> assign_new(:current_user, fn -> current_user end)
+      |> CreateCart.execute(cart_id)
+
+    {:cont, socket}
+  end
 
   defp assign_user(socket, :admin, user_token) do
     user_token
